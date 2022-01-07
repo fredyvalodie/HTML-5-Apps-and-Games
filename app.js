816b19c5d446bd58d01e42ab98bce81696dbef72
fredyvalodie <fredyvalodie@gmail.com>
2022-01-07 08:56:41 +0000

    init_drag_and_drop_file_07/01/22_08-56

--- app.js
+++ app.js
@@ -8,23 +8,35 @@ window.onload = function() {
    }
 
    function dropElement(event){
+       event.stopPropagation();
+       event.preventDefault();
         let li = document.createElement("li");
         let list = document.querySelector(".dropContainer");
-        let data = event.dataTransfer.getData("fruit");
-        let data2 = event.dataTransfer.getData("text/plain");
+        let data = event.dataTransfer.getData("text/plain");
+        let data2 = event.dataTransfer.files;
         if(data){
             li.innerHTML = data + " copied";
+            list.appendChild(li);
         }else{
-            li.innerHTML = data2 + " copied";
+            for(let i = 0; i < data2.length; i++){
+                let li_tmp = document.createElement("li");
+                li_tmp.innerHTML = data2[i].name + " copied";
+                list.appendChild(li_tmp);
+            }
         }
-        list.appendChild(li);
+        
        
    }
 
-   dragStart.ondragstart = dragElement;
+   //dragStart.ondragstart = dragElement;
    dragEnd.ondragover = function(event) {
+       dragEnd.classList.add("dragOver");
        return false;
    }
+   dragEnd.ondragleave = function(event) {
+    dragEnd.classList.remove("dragOver");
+    return false;
+}
    dragEnd.ondrop= dropElement;
 
 
