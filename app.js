3db4ce196134a8a1f45e454a2dc94b86cffff099
fredyvalodie <fredyvalodie@gmail.com>
2022-01-11 17:22:21 +0000

    creating_db_11/01/22_17-22

--- app.js
+++ app.js
@@ -1,79 +1,54 @@
 window.onload = function() {
     console.log("Page loaded !");
-    let play = document.querySelector(".play");
-    let stop = document.querySelector(".stop");
-    let download = document.querySelector(".download");
-    let progress = document.querySelector("#progressBar");
-    //console.log(progress.value);
-    let context = new window.AudioContext();
-    let source = null;
-    let audioBuffer = null;
+    // creating indexDB database
+    let users;
+    let db;
 
-    download.addEventListener("click", loadSong);
-    //play.addEventListener("click", playSong);
-    play.onclick = playSong;
-    stop.onclick = stopSong;
+    createOrOpenIndexDB()
 
+}
 
-    function loadSong() {//loadSong
-        let url = "https://mainline.i3s.unice.fr/mooc/LaSueur.mp3";
-        // xhr2 testing
-        let xhr = new XMLHttpRequest();
-        // xhr config
-        xhr.open("GET",url,true);
-        xhr.responseType = "arraybuffer";
-        xhr.onload = function(e) {
-            console.log("Download complete, decoding...");
-            prepareSong(this.response);
-        };
-        xhr.onprogress = function(e) {
-            progress.disabled = false;
-            progress.value = e.loaded;
-            progress.max = e.total;
-            console.log("Downloading song...");
-        };
-        xhr.onerror = function() {
-            console.log("Error downloading song");
-        };
-        // xhr send
-        xhr.send();
-    }
-    //decode sond data
-    function prepareSong(song) {
-        context.decodeAudioData(song, 
-            // decode success
-            function(buffer) {
-            console.log("Song decoded");
-            audioBuffer = buffer;
-            play.disabled = false;
-            stop.disabled = false;
-            }, 
-            // decode error
-            function(err){
-                console.log("Error found : " + err);
-            }
-        );
+function createOrOpenIndexDB() {
+    let request = window.indexedDB.open("Base", 1);
+
+    request.onupgradeneeded = function(e) {
+        console.log("Database not exist, creating...");
+        db = e.target.result;
+        console.log("Creating users object store");
+        users = db.createObjectStore("users", {keyPath: "id"});
+        users.createIndex("raison_sociale", "raison_sociale", {unique: true});
+        users.createIndex("numero", "numero", {unique: true});
+        // adding value in the users store
+        users.add({
+            id: 1,
+            raison_sociale: "Ets La joie",
+            domaine_activité: "commerce",
+            nature: "vente de parfums",
+            adresse: "ketao, CEG",
+            nom: "PALAKI",
+            prenom: "esso",
+            tel: "90020304",
+            email: "NEANT",
+            type: "cni",
+            numero: "012441222545",
+            date: "28/04/2018"
+        });
+    };
+
+    request.onsuccess = function(e) {
+        console.log("Database successfully loaded");
+        db = e.target.result;
+        /* adding value in the users store
+        let transaction = db.transaction["users", readwrite];
+        transaction.add({
+            //id: 1,
+            username: "Fred",
+            email: "fred@example.com",
+            gender: "male"
+        });*/
     }
 
-    function playSong(){
-        // config
-        source = context.createBufferSource();
-        source.buffer = audioBuffer;
-        source.loop = false;
-        // connect to speakers
-        source.connect(context.destination);
-        source.start(0); // play
+    request.onerror = function(e) {
+        console.log("Error while loading databse");
     }
-
-    function stopSong() {
-        if(source){
-            source.stop();
-        }
-    }
-
-
-
-
-
-
 }
